d70865672531dc58cc94355ab49c8cf81d27cdfa
thiagoopg <thiago.a.o.lima@gmail.com>
2026-03-18 16:22:36 -0300

    chore(config): ajustar playwright para reter detalhes somente ao falhar

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,9 +27,10 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://front.serverest.dev/login',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
