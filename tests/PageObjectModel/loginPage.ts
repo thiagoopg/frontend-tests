68b93c4964c8bd6083db55fbaab2fbbbfa75d472
thiagoopg <thiago.a.o.lima@gmail.com>
2026-03-18 16:21:51 -0300

    chore(tests): adicionar PageObjectModel para a página de login

---
+++ tests/PageObjectModel/loginPage.ts
@@ -0,0 +1,36 @@
+import {Locator, Page} from '@playwright/test'
+
+export class LoginPage {
+    private readonly page :Page;
+    private readonly url :string;
+    private readonly alert :Locator;
+    private readonly email :Locator;
+    private readonly password :Locator;
+    private readonly loginButton :Locator;
+    private readonly cadastrarButton :Locator;
+
+    constructor(page :Page) {
+        this.page = page;
+        this.url = 'https://front.serverest.dev/login';
+        this.alert = page.locator('.alert');
+        this.email = page.getByTestId('email');
+        this.password = page.getByTestId('password');
+        this.loginButton = page.getByTestId('entrar');
+        this.cadastrarButton = page.getByTestId('cadastrar');
+    }
+    async visitarPaginaLogin(){
+        await this.page.goto(this.url);
+    }
+    async visitarPaginaCadastroPeloLogin(){
+        await this.cadastrarButton.click();
+    }
+    async logarUsuario(email: string, password: string) {
+        await this.email.fill(email);
+        await this.password.fill(password);
+        await this.loginButton.click();
+    }
+    async getAlert():Promise<Locator>{
+        return this.alert;
+    }
+
+}
