649015aff5527f7f5f26037b964b838122a55b61
thiagoopg <thiago.a.o.lima@gmail.com>
2026-03-23 20:31:32 -0300

    chore(tests): aumentar as pages na Fixture.ts

--- tests/fixtures/Fixture.ts
+++ tests/fixtures/Fixture.ts
@@ -1,22 +1,38 @@
 import { test as base } from '@playwright/test';
 import {LoginPage} from "../PageObjectModel/loginPage";
 import {CadastroPage} from "../PageObjectModel/cadastroPage";
+import {InicialPage} from "../PageObjectModel/admin/InicialPage";
+import {CadastrarUsersPage} from "../PageObjectModel/admin/CadastrarUsersPage";
+import {ListarUsersPage} from "../PageObjectModel/ListarUsersPage";
 
 type MyFixture =
 {
     cadastroPage: CadastroPage;
     loginPage: LoginPage;
+    inicialPage: InicialPage;
+    cadastrarUsersPage:CadastrarUsersPage;
+    listarUsersPage:ListarUsersPage;
 }
 
 export const test = base.extend<MyFixture>({
     cadastroPage: async ({page}, use) => {
         const cadastroPage = new CadastroPage(page);
-        await cadastroPage.visitarPaginaCadastro();
         await use(cadastroPage);
     },
     loginPage: async ({page}, use) => {
         const loginPage = new LoginPage(page);
-        await loginPage.visitarPaginaLogin();
+        await use(loginPage);
+    },
+    inicialPage: async ({page}, use) => {
+        const loginPage = new InicialPage(page);
+        await use(loginPage);
+    },
+    cadastrarUsersPage: async ({page}, use) => {
+        const loginPage = new CadastrarUsersPage(page);
+        await use(loginPage);
+    },
+    listarUsersPage: async ({page}, use) => {
+        const loginPage = new ListarUsersPage(page);
         await use(loginPage);
     }
 })
