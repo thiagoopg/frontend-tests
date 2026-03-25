cc783ae4af6d95b4d5223c456891413b0b69af64
thiagoopg <thiago.a.o.lima@gmail.com>
2026-03-25 08:50:02 -0300

    chore(tests): adicionar beforeEach e reutilizar usuários na criação com email duplicado

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,6 +2,9 @@ import {test, expect} from './fixtures/fixture';
 import {UserFactory} from "./factory/userFactory";
 
 test.describe('criar usuários com sucesso', () => {
+    test.beforeEach(async ({cadastroPage}) => {
+        await cadastroPage.visitarPaginaCadastro()
+    })
     test('como um visitante, crio um usuário administrador com sucesso', async ({cadastroPage, page}) => {
         await cadastroPage.cadastro.cadastrarUsuario(UserFactory.build({admin: true}))
         await expect(page).toHaveURL('https://front.serverest.dev/admin/home', {timeout: 20000})
@@ -12,12 +15,18 @@ test.describe('criar usuários com sucesso', () => {
     })
 })
 test.describe('testar falhas ao criar usuários', () => {
-    test('como um visitante, crio um usuário com email repetido', async ({cadastroPage, page}) => {
-        await cadastroPage.cadastro.cadastrarUsuario(UserFactory.build({email:'repetido@qa.com',admin: false}))
-        await expect(page).toHaveURL('https://front.serverest.dev/home', {timeout: 20000})
+    test('como um visitante, crio um usuário com email repetido', async ({listarUsersPage ,cadastroPage, loginPage, page}) => {
+
+        const user = UserFactory.build({admin: false})
+
         await cadastroPage.visitarPaginaCadastro()
-        await cadastroPage.cadastro.cadastrarUsuario(UserFactory.build({email:'repetido@qa.com',admin: false}))
+        await cadastroPage.cadastro.cadastrarUsuario(user)
+        await expect(page).toHaveURL('https://front.serverest.dev/home', {timeout: 20000})
+
+        await cadastroPage.visitarPaginaCadastro()
+        await cadastroPage.cadastro.cadastrarUsuario(user)
         await expect(page).toHaveURL('https://front.serverest.dev/cadastrarusuarios')
         await expect(await cadastroPage.cadastro.getAlert()).toContainText('Este email já está sendo usado');
+
     })
 })
